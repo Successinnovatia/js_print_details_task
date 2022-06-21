067825691f8a4f8c237c87bb36893311d60af7c2
Success Divine Onyechinemere <divinesuccess228@gmail.com>
2022-06-21 23:59:03 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Program to print something
+ 
+const Name = "Divine";
+const country = " Nigeria";
+const height = "5ft 10";
+
+//using the console
+console.log("My name is "+Name + "\n" + "I am from "+country +"\n" + "My height is "+height)
+
+//using alert to show output on screen
+window.alert("My name is "+Name + "\n" + "I am from "+country +"\n" + "My height is "+height);
